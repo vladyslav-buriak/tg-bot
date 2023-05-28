5b215295fd378e3a528e9affb9daf9ed038137f1
vladyslav-buriak <vladka910@gmail.com>
2023-05-28 17:53:16 +0300

    new component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+const { default: Button } = require("../Button")
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+
+    const onClose = () => {
+        tg.close();
+    }
+
+    return (
+        <div className={"header"}>
+            <Button onClick={onClose}>Закрити</Button>
+            <span className={'username'}>{tg.initDataUnsafe?.user?.username}</span>
+        </div>
+    )
+}
+
+export default Header
