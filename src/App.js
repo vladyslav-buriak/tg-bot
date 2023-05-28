b0ba4266d86235969b3a120eda185dbdf1e90060
vladyslav-buriak <vladka910@gmail.com>
2023-05-28 18:24:40 +0300

    new component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={onToggleButton}>закрити</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
